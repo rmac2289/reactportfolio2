1f62530d488a1c071c1a84b0cc6d2441e06dd6af
Ross MacDonald <rmac2289@gmail.com>
2020-09-20 02:22:49 -0700

    changed font pairing

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -7,8 +7,8 @@ export default function Header() {
   return (
     <header className="banner" role="banner">
       <div className="header-box">
-        <h1 className="header"><strong>Ross</strong><br/> MacDonald</h1>
-        <h2 className="sub-header">full stack web developer
+        <h1 className="header"><strong style={{color: "red"}}>Ross</strong><br/> MacDonald</h1>
+        <h2 className="sub-header">full-stack web developer
         </h2>
         <div className="arrow">
           <h4
