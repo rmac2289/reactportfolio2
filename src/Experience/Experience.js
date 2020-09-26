a4847a96d62c5358590df1a706f00772772bee1f
Ross MacDonald <rmac2289@gmail.com>
2020-09-25 23:48:26 -0700

    refactored experience component & removed repetitive code

--- src/Experience/Experience.js
+++ src/Experience/Experience.js
@@ -3,25 +3,9 @@ import "../About/About.css";
 import "./Experience.css";
 import Nav from "../Nav/Nav";
 import Burger from "../Burger/Burger";
-import { img } from "@fortawesome/react-fontawesome";
 import Tooltip from "@material-ui/core/Tooltip";
 import { withStyles } from "@material-ui/core/styles";
-import reactIcon from "../images/react100px.png";
-import nodeIcon from "../images/node96px.png";
-import jsIcon from "../images/js96px.png";
-import htmlIcon from "../images/html96px.png";
-import cssIcon from "../images/css96px.png";
-import gitIcon from "../images/git96px.png";
-import githubIcon from "../images/github96px.png";
-import jqueryIcon from "../images/jquery100px.png";
-import postgresIcon from "../images/postgres96px.png";
-import terminalIcon from "../images/terminal96px.png";
-import wordpressIcon from "../images/wordpress96px.png";
-import appStoreIcon from "../images/appstore96px.png";
-import angularIcon from "../images/angular96px.png";
-import pythonIcon from "../images/python96px.png";
-import cpanelIcon from "../images/cpanel96px.png";
-import graphql from "../images/graphql.svg";
+import { tooltipData, tooltipLearningData } from "./tooltipData";
 
 const LightTooltip = withStyles((theme) => ({
   tooltip: {
@@ -29,12 +13,38 @@ const LightTooltip = withStyles((theme) => ({
     backgroundColor: theme.palette.common.white,
     color: "black",
     boxShadow: theme.shadows[1],
-    fontFamily: "Quattrocento",
+    fontFamily: "Karla, sans-serif",
     fontSize: 14,
     margin: 0,
     fontWeight: 900,
   },
 }))(Tooltip);
+
+const tooltipMap = (list) => {
+  return list.map((v) => {
+    return (
+      <LightTooltip
+        key={v.title}
+        title={v.title}
+        TransitionProps={{ timeout: 650 }}
+        placement="top"
+        arrow
+      >
+        {v.title !== "Express.js" ? (
+          <li className="tech">
+            <img className="tech-list-icon" src={v.src} alt={v.alt} />
+          </li>
+        ) : (
+          <li className="tech" id="express">
+            Express.js
+          </li>
+        )}
+      </LightTooltip>
+    );
+  });
+};
+const techList = tooltipMap(tooltipData);
+const techLearningList = tooltipMap(tooltipLearningData);
 
 export default function Experience() {
   return (
@@ -71,225 +81,9 @@ export default function Experience() {
                 into a career in web development.
               </p>
               <h4>tech I've worked with</h4>
-              <ul className="techlist">
-                <LightTooltip
-                  title="React / React Native"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={reactIcon}
-                      alt="react"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Node.js"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img className="tech-list-icon" src={nodeIcon} alt="node" />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="JavaScript"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img className="tech-list-icon" src={jsIcon} alt="js" />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="HTML5"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img className="tech-list-icon" src={htmlIcon} alt="html" />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="CSS3"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img className="tech-list-icon" src={cssIcon} alt="css" />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Git"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img className="tech-list-icon" src={gitIcon} alt="git" />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="GitHub"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={githubIcon}
-                      alt="github"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Express.js"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech" id="express">
-                    Express.js
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="jQuery"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={jqueryIcon}
-                      alt="jquery"
-                    ></img>
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="PostgreSQL"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={postgresIcon}
-                      alt="postgres"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Terminal"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={terminalIcon}
-                      alt="terminal"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="iTunes App Store"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={appStoreIcon}
-                      alt="app store"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Cpanel"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={cpanelIcon}
-                      alt="cpanel"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Wordpress"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={wordpressIcon}
-                      alt="wordpress"
-                    />
-                  </li>
-                </LightTooltip>
-              </ul>
+              <ul className="techlist">{techList}</ul>
               <h4>tech I'm learning</h4>
-              <ul className="techlist">
-                <LightTooltip
-                  title="Python"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={pythonIcon}
-                      alt="python"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="Angular"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={angularIcon}
-                      alt="angular"
-                    />
-                  </li>
-                </LightTooltip>
-                <LightTooltip
-                  title="GraphQL"
-                  TransitionProps={{ timeout: 650 }}
-                  placement="top"
-                  arrow
-                >
-                  <li className="tech">
-                    <img
-                      className="tech-list-icon"
-                      src={graphql}
-                      alt="angular"
-                    />
-                  </li>
-                </LightTooltip>
-              </ul>
+              <ul className="techlist">{techLearningList}</ul>
             </section>
           </div>
         </main>
---
+++ src/Experience/tooltipData.js
@@ -0,0 +1,107 @@
+import reactIcon from "../images/react100px.png";
+import nodeIcon from "../images/node96px.png";
+import jsIcon from "../images/js96px.png";
+import htmlIcon from "../images/html96px.png";
+import cssIcon from "../images/css96px.png";
+import gitIcon from "../images/git96px.png";
+import githubIcon from "../images/github96px.png";
+import jqueryIcon from "../images/jquery100px.png";
+import postgresIcon from "../images/postgres96px.png";
+import terminalIcon from "../images/terminal96px.png";
+import wordpressIcon from "../images/wordpress96px.png";
+import appStoreIcon from "../images/appstore96px.png";
+import cpanelIcon from "../images/cpanel96px.png";
+import angularIcon from "../images/angular96px.png";
+import pythonIcon from "../images/python96px.png";
+import graphql from "../images/graphql.svg";
+
+export const tooltipData = [
+  {
+    title: "React / React Native",
+    src: reactIcon,
+    alt: "React",
+  },
+  {
+    title: "Node.js",
+    src: nodeIcon,
+    alt: "Node.js",
+  },
+  {
+    title: "JavaScript",
+    src: jsIcon,
+    alt: "JavaScript",
+  },
+  {
+    title: "HTML5",
+    src: htmlIcon,
+    alt: "HTML5",
+  },
+  {
+    title: "CSS3",
+    src: cssIcon,
+    alt: "CSS3",
+  },
+  {
+    title: "Git",
+    src: gitIcon,
+    alt: "Git",
+  },
+  {
+    title: "GitHub",
+    src: githubIcon,
+    alt: "GitHub",
+  },
+  {
+    title: "Express.js",
+    alt: "Express.js",
+    id: "express",
+  },
+  {
+    title: "jQuery",
+    src: jqueryIcon,
+    alt: "jQuery",
+  },
+  {
+    title: "PostgreSQL",
+    src: postgresIcon,
+    alt: "PostgreSQL",
+  },
+  {
+    title: "Terminal",
+    src: terminalIcon,
+    alt: "Terminal",
+  },
+  {
+    title: "Itunes App Store",
+    src: appStoreIcon,
+    alt: "Itunes App Store",
+  },
+  {
+    title: "Cpanel",
+    src: cpanelIcon,
+    alt: "Cpanel",
+  },
+  {
+    title: "Wordpress",
+    src: wordpressIcon,
+    alt: "Wordpress",
+  },
+];
+
+export const tooltipLearningData = [
+  {
+    title: "Python",
+    src: pythonIcon,
+    alt: "Python",
+  },
+  {
+    title: "Angular",
+    src: angularIcon,
+    alt: "Angular",
+  },
+  {
+    title: "GraphQL",
+    src: graphql,
+    alt: "GraphQL",
+  },
+];
